19da08c01bf48bb2186b4ee2b1a596fb01d08d3d
po-apisit <apisit.ton456@gmail.com>
2024-02-07 13:25:17 +0700

    Test Pubilc

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,23 @@
+# เลือกเวอร์ชันของ Node.js เบื้องต้น
 FROM node:20
+
+# ตั้งค่าโฟลเดอร์ทำงานใน Docker container
+WORKDIR /app
+
+# คัดลอกไฟล์ package.json และ package-lock.json เพื่อติดตั้ง dependencies
+COPY package*.json ./
+
+# ติดตั้ง dependencies โดยใช้ npm
+RUN npm install
+
+# คัดลอกโค้ดและไฟล์ของโปรเจคเข้าไปใน Docker container
+COPY . .
+
+# สร้างโค้ด TypeScript เป็น JavaScript
+RUN npm run build
+
+# โปรดตรวจสอบว่าพอร์ตที่กำหนดใช้งานได้
+EXPOSE 4000
+
+# เริ่มต้นแอปพลิเคชัน
+CMD [ "node", "./dist/server.js" ]
--- src/connection/database-connection.ts
+++ src/connection/database-connection.ts
@@ -1,19 +1,22 @@
 import mysql from 'mysql2/promise';
 import { Request, Response, NextFunction } from 'express';
 import dotenv from 'dotenv';
-import { NOK, Network_Error } from '../installer/instant';
+
 dotenv.config();
+
+const config = {
+    connectionLimit : 100,
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT || '3306'),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+}
 
 
 const connection = async () => {
     try {
-        const conn = await mysql.createConnection({
-            host: process.env.DB_HOST,
-            port: parseInt(process.env.DB_PORT || '3306'),
-            user: process.env.DB_USER,
-            password: process.env.DB_PASSWORD,
-            database: process.env.DB_DATABASE,
-        });
+        const conn = await mysql.createConnection(config);
         return conn;
     } catch (error) {
         throw error;
@@ -26,10 +29,12 @@ const status_connection = async (req:Request, res:Response, next:NextFunction) =
         // เชื่อมต่อกับฐานข้อมูลและทดสอบการเชื่อมต่อ
         await (await connection()).ping();
         // หากสามารถเชื่อมต่อได้โดยไม่มีปัญหา
+        console.log({ connection: "database is connected"  });
+        
         return next();
     } catch (error) {
         // หากเกิดข้อผิดพลาดในการเชื่อมต่อ
-        return res.status(400).json({ result: "Fail", message: error });
+        return res.status(400).json({ connection: "Fail",  error, config });
     }
 }
 
